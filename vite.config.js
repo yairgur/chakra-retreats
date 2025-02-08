ee459130f154d342280611913365ce4021ab1c9f
Yaga <Yair.gur.arieh@gmail.com>
2025-02-08 13:05:47 +0200

    Add redirects and fix deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+         copy({
+    targets: [{ src: '_redirects', dest: 'dist' }]
+  })
+],
   build: {
     outDir: 'dist',
   },
 })
+
